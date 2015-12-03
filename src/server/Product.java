e7ba68fa054d0f03b883c04d4e6bf23785ee9003
mesrbn <me.srbn@yahoo.com>
2015-12-03 19:38:31 +0100

    serverProduct

--- src/server/Product.java
+++ src/server/Product.java
@@ -1,5 +1,19 @@
 package server;
 
 public class Product {
-
+	private String name;
+	public String getName(){
+		return this.name;
+		}
+	private Integer price;
+	public Integer getPrice(){
+		return this.price;
+		
+	}
+	private Integer availableAmount;
+	public Integer getAvailableAmount(){
+		return this.availableAmount;
+	}
+	
+   
 }
